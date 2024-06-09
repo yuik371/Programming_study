0dd8b280e4c84587bb9d59a455df36c11699c4fd
dev_yi0712 <81359957+yuik371@users.noreply.github.com>
2024-06-09 20:23:04 +0900

    [feature] Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create an instance of the Flask application.
+app = Flask(__name__)
+
+# Define a basic route.
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+# Run the application.
+if __name__ == '__main__':
+    app.run(debug=True)
